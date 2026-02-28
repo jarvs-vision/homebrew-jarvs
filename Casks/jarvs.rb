37fe993221b0e09cc3c4adb0500d58d45a6baf4d
Julien Fouilhé <julien.fouilhe@gmail.com>
2026-02-28 20:07:36 +0100

    Update JarVS cask to v0.21

--- Casks/jarvs.rb
+++ Casks/jarvs.rb
@@ -1,13 +1,13 @@
 cask "jarvs" do
-  version "0.20"
+  version "0.21"
 
   on_arm do
     url "https://github.com/jarvs-vision/jarvs-releases/releases/download/v#{version}/JarVS-arm64.dmg"
-    sha256 "5f36b8b98a294cca168baec28287aab8caedf70735f06548019bb938dd5c6c25"
+    sha256 "397246153a82c2333ddb640980b9b86e252703b438928fbf4d16568babfa5eed"
   end
   on_intel do
     url "https://github.com/jarvs-vision/jarvs-releases/releases/download/v#{version}/JarVS-x86_64.dmg"
-    sha256 "2b4b19ee53dac6ebd766decb7f0bc73bb81a8a3aa6cfa3a9808c78cb087e35ce"
+    sha256 "151aed4d820f72fac85b403b352938f5c4fa82d3abc425b4172d2fbea3bc53af"
   end
 
   name "JarVS"
